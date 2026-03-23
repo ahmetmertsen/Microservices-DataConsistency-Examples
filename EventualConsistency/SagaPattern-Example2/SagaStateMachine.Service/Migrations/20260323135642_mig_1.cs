ae029a5787ceaa9c47f1e32ec9a209b7c5957609
ahmetmert <ahmetmert0505@gmail.com>
2026-03-23 16:59:34 +0300

    feat : SagaPattern-Example2 -> StateMachine stateler ve eventler tanımlandı.

---
+++ EventualConsistency/SagaPattern-Example2/SagaStateMachine.Service/Migrations/20260323135642_mig_1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SagaStateMachine.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderStateIstance",
+                columns: table => new
+                {
+                    CorrelationId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CurrentState = table.Column<string>(type: "text", nullable: false),
+                    OrderId = table.Column<long>(type: "bigint", nullable: false),
+                    BuyerId = table.Column<long>(type: "bigint", nullable: false),
+                    TotalPrice = table.Column<decimal>(type: "numeric", nullable: false, defaultValue: 0m),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderStateIstance", x => x.CorrelationId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderStateIstance");
+        }
+    }
+}
--- EventualConsistency/SagaPattern-Example2/SagaStateMachine.Service/StateMachines/OrderStateMachine.cs
+++ EventualConsistency/SagaPattern-Example2/SagaStateMachine.Service/StateMachines/OrderStateMachine.cs
@@ -1,6 +1,10 @@
 ﻿using MassTransit;
 using SagaStateMachine.Service.StateIstances;
+using Shared.Messages;
 using Shared.OrderEvents;
+using Shared.PaymentEvents;
+using Shared.Settings;
+using Shared.StockEvents;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +16,96 @@ namespace SagaStateMachine.Service.StateMachines
     public class OrderStateMachine : MassTransitStateMachine<OrderStateIstance>
     {
         public Event<OrderStartedEvent> OrderStartedEvent { get; set; }
+        public Event<StockReservedEvent> StockReservedEvent { get; set; }
+        public Event<StockNotReservedEvent> StockNotReservedEvent { get; set; }
+        public Event<PaymentCompletedEvent> PaymentCompletedEvent { get; set; }
+        public Event<PaymentFailedEvent> PaymentFailedEvent { get; set; }
+
+        public State OrderCreated { get; set; }
+        public State StockReserved { get; set; }
+        public State StockNotReserved { get; set; }
+        public State PaymentCompleted { get; set; }
+        public State PaymentFailed { get; set; }
 
         public OrderStateMachine()
         {
             InstanceState(instance => instance.CurrentState);
+
+            Event(() => OrderStartedEvent,
+                orderStateIntance => orderStateIntance.CorrelateBy<long>(database => database.OrderId, @event => @event.Message.OrderId)
+                    .SelectId(e => Guid.NewGuid()));
+
+            Event(() => StockReservedEvent,
+                orderStateIstance => orderStateIstance.CorrelateById(@event => @event.Message.CorrelationId));
+
+            Event(() => StockNotReservedEvent,
+                orderStateIstance => orderStateIstance.CorrelateById(@event => @event.Message.CorrelationId));
+
+            Event(() => PaymentCompletedEvent,
+                orderStateIstance => orderStateIstance.CorrelateById(@event => @event.Message.CorrelationId));
+
+            Event(() => PaymentFailedEvent,
+                orderStateIstance => orderStateIstance.CorrelateById(@event => @event.Message.CorrelationId));
+
+            Initially(When(OrderStartedEvent)
+                .Then(context =>
+                {
+                    context.Instance.OrderId = context.Data.OrderId;
+                    context.Instance.BuyerId = context.Data.BuyerId;
+                    context.Instance.TotalPrice = context.Data.TotalPrice;
+                    context.Instance.CreatedAt = DateTime.UtcNow;
+                })
+                .TransitionTo(OrderCreated)
+                .Send(new Uri($"queue:{RabbitMQSettings.Stock_OrderCreatedEventQueue}"),
+                context => new OrderCreatedEvent(context.Instance.CorrelationId)
+                {
+                    OrderItems = context.Data.OrderItems
+                }));
+
+            During(OrderCreated,
+                When(StockReservedEvent)
+                .TransitionTo(StockReserved)
+                .Send(new Uri($"queue:{RabbitMQSettings.Payment_StartedEventQueue}"),
+                context => new PaymentStartedEvent(context.Instance.CorrelationId)
+                {
+                    TotalPrice = context.Instance.TotalPrice,
+                    OrderItems = context.Data.OrderItems
+                }),
+                When(StockNotReservedEvent)
+                .TransitionTo(StockNotReserved)
+                .Send(new Uri($"queue:{RabbitMQSettings.Order_OrderFailedEventQueue}"),
+                context => new OrderFailedEvent
+                {
+                    OrderId = context.Instance.OrderId,
+                    Message = context.Data.Message
+                }));
+
+            During(StockReserved,
+                When(PaymentCompletedEvent)
+                .TransitionTo(PaymentCompleted)
+                .Send(new Uri($"queue:{RabbitMQSettings.Order_OrderCompletedEventQueue}"),
+                context => new OrderCompletedEvent
+                {
+                    OrderId = context.Instance.OrderId
+                })
+                .Finalize(),
+                When(PaymentFailedEvent)
+                .TransitionTo(PaymentFailed)
+                .Send(new Uri($"queue:{RabbitMQSettings.Order_OrderFailedEventQueue}"),
+                context => new OrderFailedEvent
+                {
+                    OrderId = context.Instance.OrderId,
+                    Message = context.Data.Message
+                })
+                .Send(new Uri($"queue:{RabbitMQSettings.Stock_RollbackMessageQueue}"),
+                context => new StockRollbackMessage
+                {
+                    OrderItems = context.Data.OrderItems
+                }));
+
+            SetCompletedWhenFinalized();
+
+
         }
     }
 }
