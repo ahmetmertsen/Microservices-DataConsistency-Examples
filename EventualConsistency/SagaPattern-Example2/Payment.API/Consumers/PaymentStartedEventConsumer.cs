de9c9d078996d1c799755fcbcca670d07f8b7a89
ahmetmert <ahmetmert0505@gmail.com>
2026-03-23 17:45:23 +0300

    feat: SagaPattern-Example2 -> PaymentAPI ve Consumer sınıfları yazıldı.

---
+++ EventualConsistency/SagaPattern-Example2/Payment.API/Consumers/PaymentStartedEventConsumer.cs
@@ -0,0 +1,40 @@
+﻿using MassTransit;
+using Shared.PaymentEvents;
+using Shared.Settings;
+
+namespace Payment.API.Consumers
+{
+    public class PaymentStartedEventConsumer : IConsumer<PaymentStartedEvent>
+    {
+        private readonly ISendEndpointProvider _sendEndpointProvider;
+
+        public PaymentStartedEventConsumer(ISendEndpointProvider sendEndpointProvider)
+        {
+            _sendEndpointProvider = sendEndpointProvider;
+        }
+
+        public async Task Consume(ConsumeContext<PaymentStartedEvent> context)
+        {
+            var sendEndpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri($"queue:{RabbitMQSettings.StateMachineQueue}"));
+
+            var paymentOk = Random.Shared.Next(1, 21) > 10;
+            if (paymentOk)
+            {
+                PaymentCompletedEvent paymentCompletedEvent = new(context.Message.CorrelationId)
+                {
+
+                };
+                sendEndpoint.Send(paymentCompletedEvent);
+            }
+            else
+            {
+                PaymentFailedEvent paymentFailedEvent = new(context.Message.CorrelationId)
+                {
+                    Message = "Yetersiz bakiye...",
+                    OrderItems = context.Message.OrderItems
+                };
+                sendEndpoint.Send(paymentFailedEvent);
+            }
+        }
+    }
+}
